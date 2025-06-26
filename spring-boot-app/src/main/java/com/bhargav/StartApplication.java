c9aae736235723e68cd3e1635f25217add5c5cdf
bhargavqwertyuiop <129153182+bhargavqwertyuiop@users.noreply.github.com>
2025-06-26 10:41:23 +0530

    Update StartApplication.java

--- spring-boot-app/src/main/java/com/bhargav/StartApplication.java
+++ spring-boot-app/src/main/java/com/bhargav/StartApplication.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@SpringBootApplication
+@SpringBootApplication 
 @Controller
 public class StartApplication {
 
